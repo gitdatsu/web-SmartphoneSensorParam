e507a4baf8beae9e01bc9d89f374d35913677c54
KN <cy14144@shibaura-it.ac.jp>
2018-03-01 10:07:49 +0900

    edit deviceOrientation.js

--- docs/js/deviceOrientation.js
+++ docs/js/deviceOrientation.js
@@ -4,9 +4,8 @@ window.addEventListener("deviceorientation", function(event){
   let roll = event.gamma;
 
   let result = document.getElementById("result");
-  result.innerHTML = orientation;
-  // result.innerHTML = "方位(°)<br />"+
-  //   "方位（東西南北:0～360）："+ orientation.toFixed(1) +"<br />" +
-  //   "ピッチ（x軸回りの回転角度:-90～90）："+ pitch.toFixed(1) +"<br />" +
-  //   "ロール（y軸回りの回転角度：-90～270）："+ roll.toFixed(1);
+  result.innerHTML = "方位(°)<br />"+
+    "方位（東西南北:0～360）："+ orientation.toFixed(1) +"<br />" +
+    "ピッチ（x軸回りの回転角度:-90～90）："+ pitch.toFixed(1) +"<br />" +
+    "ロール（y軸回りの回転角度：-90～270）："+ roll.toFixed(1);
 });
