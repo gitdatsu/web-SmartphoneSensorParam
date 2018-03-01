fb28a8be2b05ad7002bdf4e09a2e67b40fe18ef3
KN <cy14144@shibaura-it.ac.jp>
2018-03-01 10:39:49 +0900

    edit deviceMotion.js

--- docs/js/deviceMotion.js
+++ docs/js/deviceMotion.js
@@ -6,8 +6,8 @@ window.addEventListener("devicemotion",function(event){
 
   let result = document.getElementById("motion");
   result.innerHTML =
-    "acceleration = (" + acceleration.x + ", " + acceleration.y + ", " + acceleration.z + ")<br>" +
-    "accelerationincludingGravity = (" + aig.x + ", " + aig.y + ", " + aig.z + ")<br>" +
-    "rotationRate = (" + rotationRate.x + ", " + rotationRate.y + ", " + rotationRate.z + ")<br>" +
+    "acceleration = (" + acceleration.x.toFixed(1) + ", " + acceleration.y.toFixed(1) + ", " + acceleration.z.toFixed(1) + ")<br>" +
+    "accelerationincludingGravity = (" + aig.x.toFixed(1) + ", " + aig.y.toFixed(1) + ", " + aig.z.toFixed(1) + ")<br>" +
+    "rotationRate = (" + rotationRate.x.toFixed(1) + ", " + rotationRate.y.toFixed(1) + ", " + rotationRate.z.toFixed(1) + ")<br>" +
     "interval = " + interval;
-})
+});
