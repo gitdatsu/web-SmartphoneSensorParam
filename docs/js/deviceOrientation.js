ffd9037f896f83212158c6b4ef4423a257395aff
KN <cy14144@shibaura-it.ac.jp>
2018-03-01 12:32:23 +0900

    edit js and pde

--- docs/js/deviceOrientation.js
+++ docs/js/deviceOrientation.js
@@ -1,15 +1,15 @@
-let alpha;
-let beta;
-let gamma;
+let g_alpha;
+let g_beta;
+let g_gamma;
 
 window.addEventListener("deviceorientation", function(event){
-  alpha = event.alpha;
-  beta = event.beta;
-  gamma = event.gamma;
+  g_alpha = event.alpha;
+  g_beta = event.beta;
+  g_gamma = event.gamma;
 
   let result = document.getElementById("orientation");
   result.innerHTML = "方位(°)<br />"+
-    "方位（Z軸回りの回転角度:0～360）："+ alpha.toFixed(1) +"<br />" +
-    "ピッチ（x軸回りの回転角度:-90～90）："+ beta.toFixed(1) +"<br />" +
-    "ロール（y軸回りの回転角度：-90～270）："+ gamma.toFixed(1);
+    "方位（Z軸回りの回転角度:0～360）："+ g_alpha.toFixed(1) +"<br />" +
+    "ピッチ（x軸回りの回転角度:-90～90）："+ g_beta.toFixed(1) +"<br />" +
+    "ロール（y軸回りの回転角度：-90～270）："+ g_gamma.toFixed(1);
 });
--- docs/pde/orientation/orientation.pde
+++ docs/pde/orientation/orientation.pde
@@ -8,15 +8,15 @@ void draw(){
 
   noStroke();
   fill(0,255,0);
-  ellipse(width/2, height-beta, 50, 50);
+  ellipse(width/2, height-g_beta, 50, 50);
   fill(0,0,255);
-  ellipse(width/2 ,height-gamma, 50, 50);
+  ellipse(width/2 ,height-g_gamma, 50, 50);
   fill(255,0,0);
-  ellipse(width/2, height-alpha, 50, 50);
+  ellipse(width/2, height-g_alpha, 50, 50);
 
   stroke(255);
-  text(alpha, 20, 20);
-  text(beta, 20, 40);
+  text(g_alpha, 20, 20);
+  text(g_beta, 20, 40);
 }
 
 void drawGraph(float[] _value){
