76d573df72e68a01366b3f409e829f090a080d8b
KN <cy14144@shibaura-it.ac.jp>
2018-03-01 09:39:23 +0900

    add index.html and js

---
+++ docs/js/deviceOrientation.js
@@ -0,0 +1,11 @@
+window.addEventListener("deviceorientation", function(event){
+  let orientation = event.alpha;
+  let pitch = event.beta;
+  let roll = event.gamma;
+
+  let result = document.getElementById("result");
+  result.innerHTML = "方位(°)<br />"+
+    "方位（東西南北:0～360）："+ orientation.toFixed(1) +"<br />" +
+    "ピッチ（x軸回りの回転角度:-90～90）："+ pitch.toFixed(1) +"<br />" +
+    "ロール（y軸回りの回転角度：-90～270）："+ roll.toFixed(1);
+});
